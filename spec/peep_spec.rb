810d43134388c268fd7349529e2eba89445291a6
Robin <robindoble@gmail.com>
2014-05-11 20:39:18 +0100

    peeps must belong to a user

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,12 +1,22 @@
 require_relative 'spec_helper'
+require 'user'
+
 
 describe Peep do 
 
 	it 'demonstration of how peeps work, a peep can be created and destroyed' do
 		expect(Peep.count).to eq(0)
-		Peep.create(:peep => "This is a peep test")
+		user = User.create(:name => "robin", :user_name => "robin_user", :email => "robin@test.com", :password => "password")
+		peep = Peep.create(:peep => "This is a peep test", :user_id => user.id)
 		expect(Peep.count).to eq(1) 
 	end
+
+	it 'must belong to a user' do 
+		user = User.create(:name => "robin", :user_name => "robin_user", :email => "robin@test.com", :password => "password")
+		peep = Peep.create(:peep => "This is a peep test", :user_id => user.id)
+		expect(peep.user.name).to eq("robin")
+	end
+
 
 
 end
